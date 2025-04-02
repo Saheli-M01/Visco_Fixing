9f38d3cec57684cf48d9a27130e61daa6e88f6b2
Saheli-M01 <sahelimondal7845@gmail.com>
2025-04-02 18:06:51 +0530

    Added About section

--- src/components/Elements/About.jsx
+++ src/components/Elements/About.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-
+import "../../styles/ElementStyle/_about.scss";
 const About = () => {
   return (
-    <div className="about">
-      <h1>About Us</h1>
-    </div>
+    <section id="about">
+      <div className="container">
+        <div className="row text-center">
+          <h1 className="pb-2">About Us</h1>
+         
+          <h4>
+          At Visco, we specialize in visualizing algorithms through dynamic animations and syntax-highlighted code snippets. Our platform helps it easier to understand complex logic through clear, engaging visuals.
+          </h4>
+          <h5><i className="fa-solid fa-check me-2"></i>Watch algorithms unfold step by step with smooth, real-time visuals.</h5>
+          <h5><i className="fa-solid fa-check me-2"></i>Understand intricate processes through intuitive, structured breakdowns.</h5>
+          <h5><i className="fa-solid fa-check me-2"></i>Control execution flow, tweak inputs, and see instant results.</h5>
+        </div>
+      </div>
+    </section>
   );
 };
 
